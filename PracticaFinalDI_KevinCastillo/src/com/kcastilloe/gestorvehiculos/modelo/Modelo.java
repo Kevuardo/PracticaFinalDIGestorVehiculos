f95129bf2bf73a09f2eaa92e5211b94bffc21374
Kevin Castillo <kcastilloescudero@gmail.com>
2017-02-27 17:14:20 +0100

    Clase modelo de Modelo

---
+++ PracticaFinalDI_KevinCastillo/src/com/kcastilloe/gestorvehiculos/modelo/Modelo.java
@@ -0,0 +1,103 @@
+package com.kcastilloe.gestorvehiculos.modelo;
+
+/**
+ * Clase Java dedicada a la tabla Modelos de la BD.
+ * 
+ * @author Kevin Castillo
+ */
+public class Modelo {
+    
+    private int id_modelo = 0;
+    private String nombre_modelo = null;
+    private int id_marca = 0;
+    private int id_eficiencia = 0;
+    private float consumo_modelo = 0f; 
+    private float emisiones_modelo = 0f;
+
+    /**
+     * Constructor de Modelo para inserción de datos en BD.
+     * 
+     * @param nombreModelo El nombre del modelo.
+     * @param id_marca El id de la marca a la que pertenece el modelo.
+     * @param id_eficiencia El id de la eficiencia que le corresponde al modelo.
+     * @param consumo_modelo El consumo del modelo, expresado en litros/100km.
+     * @param emisiones_modelo Las emisiones del modleo, expresadas en gCO2/km.
+     */
+    public Modelo(String nombreModelo, int id_marca, int id_eficiencia, float consumo_modelo, float emisiones_modelo) {
+        this.nombre_modelo = nombreModelo;
+        this.id_marca = id_marca;
+        this.id_eficiencia = id_eficiencia;
+        this.consumo_modelo = consumo_modelo;
+        this.emisiones_modelo = emisiones_modelo;
+    }
+    
+    /**
+     * Constructor de Modelo para recogida de datos en BD.
+     * 
+     * @param id_modelo El id característico del modelo (clave primaria).
+     * @param nombreModelo El nombre del modelo.
+     * @param id_marca El id de la marca a la que pertenece el modelo.
+     * @param id_eficiencia El id de la eficiencia que le corresponde al modelo.
+     * @param consumo_modelo El consumo del modelo, expresado en litros/100km.
+     * @param emisiones_modelo Las emisiones del modleo, expresadas en gCO2/km.
+     */
+    public Modelo(int id_modelo, String nombreModelo, int id_marca, int id_eficiencia, float consumo_modelo, float emisiones_modelo) {
+        this.id_modelo = id_modelo;
+        this.nombre_modelo = nombreModelo;
+        this.id_marca = id_marca;
+        this.id_eficiencia = id_eficiencia;
+        this.consumo_modelo = consumo_modelo;
+        this.emisiones_modelo = emisiones_modelo;
+    }
+
+    public int getId_modelo() {
+        return id_modelo;
+    }
+
+    public void setId_modelo(int id_modelo) {
+        this.id_modelo = id_modelo;
+    }
+
+    public String getNombre_modelo() {
+        return nombre_modelo;
+    }
+
+    public void setNombre_modelo(String nombre_modelo) {
+        this.nombre_modelo = nombre_modelo;
+    }
+
+    public int getId_marca() {
+        return id_marca;
+    }
+
+    public void setId_marca(int id_marca) {
+        this.id_marca = id_marca;
+    }
+
+    public int getId_eficiencia() {
+        return id_eficiencia;
+    }
+
+    public void setId_eficiencia(int id_eficiencia) {
+        this.id_eficiencia = id_eficiencia;
+    }
+
+    public float getConsumo_modelo() {
+        return consumo_modelo;
+    }
+
+    public void setConsumo_modelo(float consumo_modelo) {
+        this.consumo_modelo = consumo_modelo;
+    }
+
+    public float getEmisiones_modelo() {
+        return emisiones_modelo;
+    }
+
+    public void setEmisiones_modelo(float emisiones_modelo) {
+        this.emisiones_modelo = emisiones_modelo;
+    }
+
+    
+    
+}
